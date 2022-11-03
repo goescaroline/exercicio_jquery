34a206dbbbdbc73513467cf313ea369f6f0f0aba
goescaroline <goes_caroline@hotmail.com>
2022-11-03 20:03:03 -0300

    criando novo projeto

---
+++ main.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('form-tarefa');
+const tarefas = [];
+
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();    
+})
+
+function adicionaLinha() {
+    const inputNomeTarefa = document.getElementById('nome-tarefa');
+
+    if (tarefas.includes(inputNomeTarefa.value)) {
+        alert(`A Tarefa: ${inputNomeTarefa.value} já foi cadastrada`);
+    } else {
+    tarefas.push(inputNomeTarefa.value);
+
+    let linha = '<ul>';
+    linha += `<li>${inputNomeTarefa.value}</li>`;
+    linha += `</ul>`;
+
+    linhas += linha;    
+    }
+
+    inputNomeTarefa.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('ul');
+    corpoTabela.innerHTML = linhas;
+}
